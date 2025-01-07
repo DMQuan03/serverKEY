c6ff6fd8d6429b9b0c285a697db2e2bb052a43c3
DMQuan03 <123900871+DMQuan03@users.noreply.github.com>
2025-01-07 11:35:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express")
 const app = express()
 const PORT = 5678
 const cors = require("cors")
-const db = require("./config/db")
 const dotenv = require("dotenv")
-const defaultRouter = require("./api/router/index")
 dotenv.config()
 app.use(cors({
     origin: "*",
@@ -13,8 +11,22 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-db.connect()
-defaultRouter(app)
+
+app.post("/shopee/api/v1/login", async (req, res) => {
+    const { cookie } = await req.body
+    const url = "https://banhang.shopee.vn/webchat/api/coreapi/v1.2/mini/login?csrf_token=P7gR2KvTEe%2BOwz6%2FdheT4Q%3D%3D&source=pcmall&_api_source=pcmall"
+    let headers = {
+        'cookie': cookie
+    }
+    const response = await axios.post(url, {}, { headers: headers })
+    console.log(response.data)
+    return res.status(200).json({
+        data: response.data,
+        success: true,
+        message: "Login success"
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`server is running http://127.0.0.1:${PORT}`)
 })
